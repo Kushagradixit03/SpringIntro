6a84894f752284a5c82316ecf63ba6ed0ad9bd7d
Kushagradixit03 <kushagradixit0312@gmail.com>
2025-03-01 21:41:56 +0530

    [Kushagra Dixit]add.UC2

--- src/main/java/com/example/demo/controller/SayHello.java
+++ src/main/java/com/example/demo/controller/SayHello.java
@@ -1,16 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class HelloMessage {
+public class SayHello {
     //curl localHost:8080/hello-w "\n"
     @RequestMapping(value = {"","/","/home"})
     public String sayHello(){
         return "Hello From Kushagra";
     }
+
+    //curl localhost:8080/hello/query?name=Kushagra -w "\n"
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+        public  String SayHello(@RequestParam(value = "name")String name) {
+        return "Hello " + name + "!";
+    }
+
 
 }
 
